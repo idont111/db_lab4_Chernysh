0f33a1fd93dc0762bae2acdf19f9bafcfa5eb4c6
Alina Chernysh <55505572+idont111@users.noreply.github.com>
2021-12-19 12:22:42 +0200

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,17 +1,16 @@
-CREATE OR REPLACE FUNCTION genre_count(genre VARCHAR(20)) 
-RETURNS int
-LANGUAGE plpgsql
-AS
-$$
-	DECLARE
-		res integer;
+CREATE OR REPLACE FUNCTION get_count_of_genre(name_of_genre varchar(20))
+RETURNS NUMERIC
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE count_genre NUMERIC;
 
-	BEGIN
-		SELECT COUNT(*) INTO res
-		FROM book
-		INNER JOIN genre ON book.genre_id = genre.genre_id
-		WHERE genre.genre_name = genre;
-
-		RETURN res;
-	END;
+BEGIN
+	count_genre := (SELECT COUNT(*) 
+					FROM book 
+					JOIN genre ON genre.genre_id = book.genre_id 
+					WHERE genre_name = name_of_genre);
+	RETURN count_genre;
+END;
 $$;
+
+SELECT get_count_of_genre('Diary fiction');
