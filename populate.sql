be2f7b074cb8b68f07168f44cd44e378568457a5
Alina Chernysh <55505572+idont111@users.noreply.github.com>
2021-12-23 23:11:08 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,16 +1,18 @@
-INSERT INTO author (author_id, first_name, last_name)
-VALUES 
-('101b', 'Jeff', 'Kinney'),
-('102b', 'Suzanne', 'Collins'),
-('103b', 'Elie', 'Wiesel'),
-('104b', 'Charlaine', 'Harris'),
-('105d', 'Walter', 'Isaacson'),
-('106b', 'Michelle', 'Obama'),
-('107d', 'Rupi', 'Kaur'),
-('108d', 'Andy', 'Weir'),
-('109c', 'James', 'Dashner');
 
-INSERT INTO genre (genre_id, genre_name)
+INSERT INTO authors (author_id, first_name, last_name)
+VALUES 
+('101', 'Jeff', 'Kinney'),
+('102', 'Suzanne', 'Collins'),
+('103', 'Elie', 'Wiesel'),
+('104', 'Charlaine', 'Harris'),
+('105', 'Walter', 'Isaacson'),
+('106', 'Michelle', 'Obama'),
+('107', 'Rupi', 'Kaur'),
+('108', 'Andy', 'Weir'),
+('109', 'James', 'Dashner'),
+('110', 'Marion', 'Wiesel');
+
+INSERT INTO genres (genre_id, genre_name)
 VALUES 
 ('01', 'Memoir'),
 ('02', 'Diary fiction'),
@@ -18,27 +20,46 @@ VALUES
 ('04', 'Fantasy Fiction'),
 ('05', 'Drama'),
 ('06', 'Poetry'),
-('07', 'Young-adult fiction');
+('07', 'Comedy');
 
-INSERT INTO rank_period(period_id, rank_period_name)
+INSERT INTO books(book_id, book_name, data_published)
 VALUES 
-('11n', to_date('2019-11-11', 'yyyy-mm-dd')),
-('12d', to_date('2009-12-12', 'yyyy-mm-dd')),
-('01j', to_date('2018-01-01', 'yyyy-mm-dd')),
-('02f', to_date('2012-02-02', 'yyyy-mm-dd'));
+('1', 'Becoming', to_date('2009-12-12', 'yyyy-mm-dd')),
+('2', 'Dog Days (Diary of a Wimpy Kid)', to_date('2018-01-01', 'yyyy-mm-dd')),
+('3', 'Double Down (Diary of a Wimpy Kid)',to_date('2012-02-02', 'yyyy-mm-dd')),
+('4', 'Old School (Diary of a Wimpy Kid )', to_date('2018-01-01', 'yyyy-mm-dd')),
+('5', 'Catching Fire (The Hunger Games)', to_date('2017-12-13', 'yyyy-mm-dd')),
+('6', 'Mockingjay (The Hunger Games)', to_date('2017-10-23', 'yyyy-mm-dd')),
+('7', 'Night (Night)', to_date('2013-05-13', 'yyyy-mm-dd')),
+('8', 'Steve Jobs', to_date('2011-06-04', 'yyyy-mm-dd')),
+('9', 'Milk and Honey',to_date('2010-04-04', 'yyyy-mm-dd')),
+('10', 'Milk and Vine',to_date('2014-03-01', 'yyyy-mm-dd'));
 
-INSERT INTO book (book_id, title, author_id, genre_id, period_id )
+INSERT INTO book_genres(id, book_id, genre_id)
 VALUES 
-(1, 'Becoming', '106b', '01', '11n'),
-(2, 'Dog Days (Diary of a Wimpy Kid)', '101b','02', '12d'),
-(3, 'Double Down (Diary of a Wimpy Kid)','101b', '02', '01j'),
-(4, 'Old School (Diary of a Wimpy Kid )', '101b', '02', '02f'),
-(5, 'Catching Fire (The Hunger Games)', '102b', '03', '11n'),
-(6, 'Mockingjay (The Hunger Games)', '102b', '03', '01j'),
-(7, 'Night (Night)', '103b', '01', '02f'),
-(8, 'Sookie Stackhouse', '104b', '04', '12d'),
-(9, 'Steve Jobs', '105d', '05', '02f'),
-(10, 'Milk and Honey','107d', '06', '11n'),
-(11, 'Milk and Vine','107d', '06', '01j'),
-(12, 'The Martian', '108d', '03', '01j'),
-(13, 'The Maze Runner','109c','07', '02f');
+('201', '1', '01'),
+('202', '2', '02'),
+('203', '2', '07'),
+('204', '3', '02'),
+('205', '4', '02'),
+('206', '5', '03'),
+('207', '6', '03'),
+('208', '6', '04'),
+('209', '7', '01'),
+('210', '8', '05'),
+('211', '9', '06'),
+('212', '10', '06');
+
+INSERT INTO book_author(id, book_id, author_id)
+VALUES 
+('301', '1', '106'),
+('302', '2', '101'),
+('303', '3', '101'),
+('304', '4', '101'),
+('305', '5', '102'),
+('306', '6', '102'),
+('307', '7', '103'),
+('308', '7', '109'),
+('309', '8', '105'),
+('310', '9', '107'),
+('311', '10', '107');
